829fe7843fb847d54030f9f9e7b6d942c86e306b
KangJeongTaek <dfgddjd@naver.com>
2025-03-18 17:54:24 +0900

    2025-03-18 오후 5시 45분 하노이의 탑

--- src/ch05/HanoiWhile.java
+++ src/ch05/HanoiWhile.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 class HanoiWhile {
     static class Move {
-        int diskNumber; // 움직여야할 원반 개수
+        int diskNumber; // 실제 디스크
         int from, to;
 
         Move(int diskNumber, int from, int to) {
@@ -16,7 +16,7 @@ class HanoiWhile {
 
     public static void solveHanoiIterative(int numDisks, int fromRod, int auxRod, int toRod) {
         Stack<Move> stack = new Stack<>();
-        Stack<Integer> disksStack = new Stack<>(); //실제 디스크
+        Stack<Integer> disksStack = new Stack<>(); // 움직일 원판의 개수
 
         // 처음 호출을 위해 numDisks, 시작, 보조, 목표 기둥을 넣습니다.
         stack.push(new Move(numDisks, fromRod, toRod));
@@ -35,8 +35,8 @@ class HanoiWhile {
                 stack.push(new Move(disks - 1, aux, move.to));
                 disksStack.push(disks - 1);
 
-                stack.push(new Move(disks, move.from, move.to));
-                disksStack.push(1);  // 실제 이동할 원반 번호 기록
+                stack.push(new Move(disks, move.from, move.to)); // 실재 디스크
+                disksStack.push(1);  // 움직일 원판의 개수
 
                 stack.push(new Move(disks - 1, move.from, aux));
                 disksStack.push(disks - 1);
